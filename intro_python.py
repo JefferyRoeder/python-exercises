05e9e7f9469b1b70cade4fd45fc869be36ef0937
JefferyRoeder <jefferyroeder@gmail.com>
2019-09-11 11:40:58 -0500

    python exercises and 17 list comprehension

--- intro_python.py
+++ intro_python.py
@@ -52,18 +52,28 @@ type({'a':[]})
 # {} + {} : error
 
 # You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?
-
-total = (3*3) + (5 * 3) + (3*1)
+mermaid_days = 3
+bear_days = 5
+hercules_days = 1
+rate = 3
+total = (mermaid_days * rate) + (bear_days * rate) + (hercules_days * rate)
 print(total)
 
 # Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+google_rate = 400
+amazon_rate = 380
+facebook_rate = 350
+google_hours = 6
+amazon_hours = 4
+facebook_hours = 10
 
-payment = (350 * 10) + (6 * 400) + (4 * 380)
-print(payment)
+total_pay = (google_rate * google_hours) + (amazon_rate * amazon_hours) + (facebook_hours * facebook_rate)
+
+print(total_pay)
 
 
 # A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
-
+# can attend
 course_seats_available = 2
 course_overlap = False
 if course_seats_available > 0 and course_overlap == False:
@@ -72,6 +82,7 @@ else:
     able_to_enroll = False
 print(able_to_enroll)
 
+# can't attend
 course_seats_available = 2
 course_overlap = True
 if course_seats_available > 0 and course_overlap == False:
@@ -82,10 +93,20 @@ print(able_to_enroll)
 
 
 # A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
-
+# discount applied
 products_purchased = 1
 offer_expiration = 8/12/2020
 premium_memeber = True
+if premium_memeber == True or (offer_expiration >= today() and products_purchased >2):
+    special_offer = True
+else:
+    special_offer = False
+print(special_offer)
+
+# discount not applied
+products_purchased = 1
+offer_expiration = 8/12/2020
+premium_memeber = False
 if premium_memeber == True or (offer_expiration >= today() and products_purchased >2):
     special_offer = True
 else:
@@ -103,7 +124,7 @@ password = 'notastrongpassword '
 # the password must be at least 5 characters
 
 password_character_requirement = len(password) >=5
-print(character_requirement)
+print(password_character_requirement)
 
 # the username must be no more than 20 characters
 
